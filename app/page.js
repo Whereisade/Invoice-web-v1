f159da93147430a3986faf1a5f06540263e95888
Whereisade <eniolaadeleke2303@gmail.com>
2025-03-04 21:35:56 +0100

    FEAT: BEgin

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import InvoiceForm from "./_component/InvoiceForm";
 
 export default function Home() {
   return (
     <div>
-      <InvoiceForm/>
+      
     
     </div>
   );
